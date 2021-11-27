2c7a86ae91ea340805148662fe8fdee0d7610c04
MAS291109 <81005913+MAS291109@users.noreply.github.com>
2021-11-27 12:00:23 +0530

    Add files via upload
    added CSS and started JS.

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 function startClassification()
 {
-    navigator.mediaDevices.getUserMedia({ audio: true})
-    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/5dKlZETIv/model.json', modelReady);
+  navigator.mediaDevices.getUserMedia({ audio: true});
+  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/O1zJlzMhc/model.json', modelReady);
 }
 function modelReady(){
     classifier.classify(gotResults);
+  }
+  function gotResults(error, results) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(results);
+    }
 }
